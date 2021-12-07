79c7e74974bd8e0bf27b6450a565e484449080e1
Prashant Kesarwani <47484727+pikukesar@users.noreply.github.com>
2021-12-07 15:28:23 -0500

    Update vm.tf

--- terraformwithversion/modules/vm/vm.tf
+++ terraformwithversion/modules/vm/vm.tf
@@ -21,7 +21,7 @@ resource "azurerm_linux_virtual_machine" "main" {
   network_interface_ids = [azurerm_network_interface.main.id]
   admin_ssh_key {
     username   = "testadmin"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file("~/.ssh-keys/id_rsa.pub")
   }
   os_disk {
     caching           = "ReadWrite"
